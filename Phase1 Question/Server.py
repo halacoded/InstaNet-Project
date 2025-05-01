6ec663d418d664c370ce7e1018d3080e338896c5
NourTech <66703154+NourMeshal@users.noreply.github.com>
2025-05-01 23:22:13 +0300

    Update Server.py

--- Phase1 Question/Server.py
+++ Phase1 Question/Server.py
@@ -4,17 +4,26 @@ import threading
 
 # === TODO Students to implement this ===
 def handle_client(textbox):
-    # TODO implementation goes here
+   # TODO implementation goes here
     # 1. Create a socket.
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Using IPV4, TCP Connection
     # 2. Bind it to localhost, port 12345.
+    s.bind(('localhost', 12345))
     # 3. Listen for one connection.
+    s.listen(1)
     # 4. Accept a client.
+    while true:
+        client_socket, address = s.accept()
     # 5. Receive message from client.
+    message = client_socket.recv(MAX_MEMORY).decode() #MAX_MEMORY returns maximum number of bytes to read from the socket at once.
     # 6. Show the message in textbox.
+    textbox.insert('end', f"Client Message: {message}\n")
     # 7. Send back "Hi Client!" as a reply.
+    client_socket.send(bytes("Hi Client!", 'utf-8'))
     # 8. Close connection and socket.
+    client_socket.close()
     pass
-
+    
 # === GUI below (already working) ===
 def start_server():
     threading.Thread(target=lambda: handle_client(textbox), daemon=True).start()
